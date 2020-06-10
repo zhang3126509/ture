862933759861169ac692d604ed8da11d9755243c
ture <1214116015@qq.com>
2020-06-10 16:48:39 +0800

    g

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,16 +1,19 @@
 compton &
 fcitx &
+xfce4-power-manager &
+sh ~/.config/polybar/polybar.sh
+xmodmap ~/.xmodmap &
 
-
-while true
-do
-	yinliang=`amixer get Master | sed -n '$p' |cut -d"]" -f1 |cut -d"[" -f2`
-	jingyin=`amixer get Master | sed -n '$p' |cut -d] -f3 |cut -d" " -f2 |cut -d"[" -f2`
-	dianyuan=`acpi |cut -d, -f2`
-	shijian=`date | cut -d" " -f1,2,3,5`
-	xsetroot -name "${dianyuan}|${shijian}|VOL:${yinliang}|${jingyin}"
-	sleep 1
-done &
+#dwm下显示信息的配置
+#while true
+#do
+#	yinliang=`amixer get Master | sed -n '$p' |cut -d"]" -f1 |cut -d"[" -f2`
+#	jingyin=`amixer get Master | sed -n '$p' |cut -d] -f3 |cut -d" " -f2 |cut -d"[" -f2`
+#	dianyuan=`acpi |cut -d, -f2`
+#	shijian=`date | cut -d" " -f1,2,3,5`
+#	xsetroot -name "${dianyuan}|${shijian}|VOL:${yinliang}|${jingyin}"
+#	sleep 1
+#done &
 #while xsetroot -name "`date``uptime | sed 's/.*,//'``vol`"
 #do
 #		sleep 1
